16dc17a8fdd307a39ba28091d77c59a86431b557
James Dixon <james.dixon@eng.ox.ac.uk>
2026-06-03 10:51:54 +0100

    propper oxford logo and added org links

---
+++ src/data/affiliations.ts
@@ -0,0 +1,12 @@
+export const affiliationUrls: Record<string, string> = {
+  'Nanyang Technological University':              'https://www.ntu.edu.sg',
+  'Max Planck Institute for Intelligent Systems':  'https://is.mpg.de/en',
+  'Microsoft Research':                            'https://www.microsoft.com/en-us/research/',
+  'University of Pennsylvania':                    'https://www.upenn.edu',
+  'Cornell University & Databricks AI':            'https://www.cornell.edu',
+  'University of Oxford':                          'https://www.ox.ac.uk',
+  'University of Warwick':                         'https://warwick.ac.uk',
+  'Stanford University':                           'https://www.stanford.edu',
+  'London School of Economics and Political Science': 'https://www.lse.ac.uk',
+  'University of Milano-Bicocca':                  'https://en.unimib.it',
+};
